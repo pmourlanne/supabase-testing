40cc9a8c265d364fa8c3a18b64c7011ac9b9fa3b
Pierre Mourlanne <pmourlanne@gmail.com>
2023-06-13 16:16:29 +0200

    First commit, following Supabase Vue tutorial

---
+++ README.md
@@ -0,0 +1,12 @@
+Testing supabase out :eyes:
+
+
+### What I did
+
+Following https://supabase.com/docs/guides/getting-started/quickstarts/vue
+
+```bash
+npm init vue@latest supabase-testing
+cd supabase-testing/
+npm install @supabase/supabase-js
+```
---
+++ supabase-testing/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://okgpvhlhhvhzcilgbjiu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZ3B2aGxoaHZoemNpbGdiaml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY2NjQ1MDcsImV4cCI6MjAwMjI0MDUwN30.5rVeq57mGAFK-Q3cPwSBsWmILQx_UOKL5hJ4IxKwczo')
